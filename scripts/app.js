137b465fd9c725f01b8655588e3d37fb08438eb8
johnson-rl <johnson.rl@gmail.com>
2016-12-06 18:09:27 -0800

    Loads top 25 images, need to hook up button.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,46 @@
 $(document).on("ready", function(){
 
+  $.ajax({
+      method: "GET",
+      url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+      datatpye: "json",
+      success: onSuccess,
+      error: onError,
+      complete: completeCallback
+  });
+
+
+
+  $('form').on('submit', function(event){
+  event.preventDefault()
+    $.ajax({
+        method: "GET",
+        url: "http://api.giphy.com/v1/gifs/search",
+        data: $("form").serialize(),
+        datatpye: "json",
+        success: onSuccess,
+        error: onError
+    });
+  });
 });
+
+function onSuccess(response) {
+    console.log(response);
+    response.data.forEach(imageGrab);
+};
+
+function imageGrab (imageObject){
+  var imageApend = imageObject.images.fixed_height.url;
+  $('.gif-gallery').append("<img src ="+imageApend+">");
+}
+
+function onError(xhr, status, errorThrown) {
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+};
+
+function completeCallback(responsData) {
+    // console.log(responsData);
+};
